63369d2b77739b8958ec388a1318b908d01de68f
Ashwani1330 <ashwanihabri@gmail.com>
2024-05-17 01:09:57 +0530

    P2_additions

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -78,17 +78,150 @@ main:
     mov ss, ax
     mov sp, 0x7C00  ; stack grows downwards from where we are loaded in memory
 
+    ; read something from floppy disc
+    ; BIOS should set dl to drive number
+    mov [ebr_drive_number], dl
+
+    mov ax, 1  ; LBA=1, second sector from disc
+    mov cl, 1  ; 1 sector to read .. 
+    mov bx, 0x7E00  ; data should be after the bootloader
+    call disk_read
+
     ; print message
     mov si, msg_hello
     call puts
 
     hlt
 
+
+;
+; Error handlers
+;
+
+floppy_error:
+    mov si, msg_read_failed 
+    call puts
+    jmp wait_key_and_reboot
+
+wait_key_and_reboot:
+    mov ah, 0
+    int 16h  ; wait for a keypress
+    jmp 0FFFFh:0  ; jump to beginning of BIOS, should reboot
+
+
 .halt:
-    jmp .halt
+    cli
+    hlt  ; disable interrupts, this way CPU can't get out of the "halt" state
+
+;
+; Disc routines
+;
+
+;
+; Converts an LBA address to CHS address
+; Parameters
+;  - ax: LBA address
+; Returns:
+;  - cx [bits 0-5]: sector number
+;  - cx [bits 6-15] : cylinder
+;  - dh : head
+;
+
+lba_to_chs:
+
+    push ax
+    push dx
+
+    xor dx, dx  ; dx = 0
+    div word [bdb_sectors_per_track]  ; ax = LBA / SectorsPerTrack
+                                      ; dx = LBA % SectorsPerTrack
+
+    inc dx  ; dx = (LBA % SectorsPerTrack + 1) = sector
+    mov cx, dx
+
+    xor dx, dx  ; dx = 0
+    div word [bdb_heads]  ; ax = (LBA / SectorsPerTrack) / Head = cylinder
+                          ; dx = (LBA / SectorsPerTrack) % Head = head
+    
+    mov dh, dl  ; dh = head
+    mov ch, al  ; ch = cylinder (lower 8 bits) 
+    shl ah, 6
+    or  cl, ah  ; put upper 2 bits of cylinder in CL
+
+    pop ax
+    mov dl, dl  ; restore DL
+    pop ax
+    ret
+
+;
+; Reads sectors from a disk
+; Parameters:
+;   - ax: LBA
+;   - cl: number of sectors to read (up to 128)
+;   - dl: drive number
+;   - ex:bx: memory address where to store read data
+;
+disk_read:
+
+    push ax  ; save registers we will modify
+    push bx
+    push cx
+    push dx
+    push di
+
+    push cx  ; temporarily save CL (number of sectors to read)
+    call lba_to_chs  ; compute CHS
+    pop ax  ; AL = number of sectors to read
+
+    mov ah, 02
+    mov di, 3  ; retry count
+
+.retry:
+    pusha  ; save all registers, we don't know what bios modifies
+    stc  ; set carry flag, some BIOS'es don't set it
+    int 13h  ; carry flag cleared = success
+    jnc .done  ; jump if carry not set
+
+    ; read failed
+    popa
+    call disk_reset
+
+    dec di
+    test di, di
+    jnz .retry
+
+.fail:
+    ; all attempts are exhausted
+    jmp floppy_error
+
+
+.done:
+    popa
+
+    push ax  ; restore registers modified
+    push bx
+    push cx
+    push dx
+    push di
+    ret
+
+;
+; Reset disc controller
+; Parameters:
+;   dl: drive number
+;
+disk_reset:
+    pusha
+    mov ah, 0
+    stc
+    int 13h
+    jc floppy_error
+    popa
+    ret
 
 
 msg_hello: db 'Hello world!', ENDL, 0
+msg_read_failed: db 'Read from disc failed!', ENDL, 0
 
 ; repeats the instruction to store the value 0 in memory (a byte) as many times 
 ; as needed to fill the remaining space in the boot  sector, up to a maximum of
